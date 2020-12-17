21c8c92065fedfd8e5ce4500a75a94a632c6ae98
tarmes <traarmes@gmail.com>
2020-12-16 21:06:22 -0500

    tests working and enhancer is defined

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,29 @@
-module.exports = {
-  success,
-  fail,
-  repair,
-  get,
-};
 
-function success(item) {
-  return { ...item };
+class Enhancer {
+  constructor(name) {
+    this.name = name
+    this.enhancement = 15
+    this.durability = 45
+  }
+  success(item) {
+    return { ...item };
+  }
+  
+  fail(item) {
+    return { ...item };
+  }
+  
+  repair(item) {
+    return { ...item };
+  }
+  
+  get(item) {
+    return { ...item };
+  }
 }
 
-function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
-}
+module.exports = Enhancer;
+// - a `repair(item)` method that accepts an `item` object and **returns a new item** with the durability restored to 100. This method is the simplest of the three and would be a **good starting point** on this project.
+// - a `success(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement success.
+// - a `fail(item)` method that accepts an `item` object and **returns a new item** object modified according to the rules defined by the client for enhancement failure.
+// - a `get()` method for use when working on the stretch problem.
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('Sanity and intro', () => {
+   it('works', () => {
+      expect(2 + 2).toBe(4)
+   })
+   it('everything working', () => {
+      expect({}).not.toBe({})
+      expect({}).toEqual({})
+   })
+})
+
+describe('Item Class', () => {
+   let item
+   beforeEach(() => {
+      item = new enhancer('mana juice')
+   })
+   it('is defined', () => {
+      expect(enhancer).toBeDefined()
+   })
+   
+})
